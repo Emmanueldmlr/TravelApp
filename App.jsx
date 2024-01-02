9db009fb9d604fa755c08df643178875bbd7ca73
Damilare Bakare <bakaredmlr@gmail.com>
2024-01-02 19:41:01 +0000

    sets up the navigator

--- App.jsx
+++ App.jsx
@@ -7,10 +7,21 @@
 
 import React from 'react';
 import Home from './src/screens/Home';
+import { NavigationContainer } from '@react-navigation/native';
+import 'react-native-gesture-handler';
+import { createStackNavigator } from '@react-navigation/stack';
 
+
+
+const Stack = createStackNavigator();
+ 
 function App() {
   return (
-    <Home/>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 export default App;
