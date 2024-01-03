527670f076be09bfc582038bcecce04ce923255b
Damilare Bakare <bakaredmlr@gmail.com>
2024-01-03 18:58:10 +0000

    instal google maps

--- ios/AwesomeProject/AppDelegate.mm
+++ ios/AwesomeProject/AppDelegate.mm
@@ -1,11 +1,14 @@
 #import "AppDelegate.h"
 
 #import <React/RCTBundleURLProvider.h>
+#import <GoogleMaps/GoogleMaps.h>
+
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  [GMSServices provideAPIKey:@"AIzaSyBUhiprOvAszMf1MC81PfI9UPU55RJy_lU"]; // add this line using the api key obtained from Google Console
   self.moduleName = @"AwesomeProject";
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
