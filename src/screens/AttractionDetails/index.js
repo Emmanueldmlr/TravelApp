1886bd6c5e8c8ed8fff13ad98cc8f201137b89d2
Damilare Bakare <bakaredmlr@gmail.com>
2024-01-03 16:18:13 +0000

    implements image Thumbnails

--- src/screens/AttractionDetails/index.js
+++ src/screens/AttractionDetails/index.js
@@ -1,12 +1,39 @@
 import React from 'react';
-import { SafeAreaView, ImageBackground } from 'react-native';
+import { SafeAreaView, ImageBackground, Image, View, Pressable, Text } from 'react-native';
 import { attractionStyle } from './styles';
-const AttractionDetails = ({route}) => {
+const AttractionDetails = ({route, navigation}) => {
   const { attraction } = route?.params || {};
   const mainImage = attraction?.images[0];
   return (
     <SafeAreaView style={attractionStyle.container}>
-      <ImageBackground source={{uri: mainImage}} imageStyle={{borderRadius: 20}} style={attractionStyle.mainImage}/>
+      <ImageBackground
+        source={{uri: mainImage}}
+        imageStyle={{borderRadius: 20}}
+        style={attractionStyle.mainImage}>
+        <View style={attractionStyle.header}>
+          <Pressable hitSlop={8} onPress={() => navigation.goBack()}>
+            <Image
+              source={require('../../assets/back.png')}
+              style={attractionStyle.icon}
+            />
+          </Pressable>
+          <Pressable hitSlop={8}>
+            <Image
+              source={require('../../assets/share.png')}
+              style={attractionStyle.icon}
+            />
+          </Pressable>
+        </View>
+        <View style={attractionStyle.footer}>
+          {attraction?.images.map((image, index) => (
+            <Image
+              source={{uri: image}}
+              style={attractionStyle.imageThumbnail}
+              key={index}
+            />
+          ))}
+        </View>
+      </ImageBackground>
     </SafeAreaView>
   );
 };
--- src/screens/AttractionDetails/styles.js
+++ src/screens/AttractionDetails/styles.js
@@ -8,5 +8,35 @@ export const attractionStyle = StyleSheet.create({
   mainImage: {
     width: '100%',
     height: 0.5 * height,
-  }
+    flexDirection: "column",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginTop: 20,
+    marginHorizontal: 40,
+    width: '90%',
+  },
+  icon: {
+    width: 40,
+    height: 40,
+  },
+  footer: {
+    flexDirection: 'row',
+    alignContent: 'center',
+    borderRadius: 20,
+    alignItems: 'center',
+    backgroundColor: 'rgba(255, 255, 255, 0.8)',
+    margin: 10,
+    flexWrap: 'wrap', 
+    justifyContent: 'center',
+  },
+  imageThumbnail: {
+    width: 30,
+    height: 30,
+    borderRadius: 10,
+    margin: 8
+  },
 });
